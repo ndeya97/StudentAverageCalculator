87cff613b574f6083e9b65cb13e91cfd589b1f03
ndeya97 <nadiop97@gmail.com>
2024-11-13 12:23:21 +0100

    method saisirEtudiant() set

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -7,7 +7,35 @@ import sn.isi.controller.Fonction;
 public class Main {
 
 	public static void main(String[] args) {
-				
+			
+		//Message de présentation
+		System.out.println("**********************************");
+		System.out.println("*********** Bienvenenue  *********");
+		System.out.println("*Dans Student Average Calculator**");
+		System.out.println("**********************************");
+		
+		//Creation d'une instance de la classe Fonction
+		Fonction Fonction = new Fonction();
+		
+		// Création d'un Scanner partagé
+	    Scanner scanner = new Scanner(System.in);
+	    
+        String continuer;
+        
+        do {
+            // Saisie des informations de l'étudiant
+            Fonction.saisirEtudiant(scanner);
+            
+	        // Demander à l'utilisateur s'il souhaite continuer
+	        System.out.print("Voulez-vous saisir un autre étudiant ? (oui/non) : ");
+	        continuer = scanner.nextLine();
+           
+        } while (continuer.equalsIgnoreCase("oui"));
+
+	    // Fermeture du scanner
+        scanner.close();
+        System.out.println("Fin du programme.");
 	}
+	
 
 }
--- src/sn/isi/controller/Fonction.java
+++ src/sn/isi/controller/Fonction.java
@@ -1,5 +1,23 @@
 package sn.isi.controller;
 
+import java.util.Scanner;
+
 public class Fonction {
+	
+    // Méthode pour saisir les informations de l'étudiant
+	public String[] saisirEtudiant(Scanner scanner){		
+		//Saisie du nom
+		System.out.print("Nom de l'étudiant : ");
+		String nom = scanner.nextLine();
+		//Saisie du prenom
+		System.out.print("Prenom de l'étudiant : ");
+		String prenom = scanner.nextLine();
+		//Saisie de la date de naissance
+		System.out.print("Date de naissance (jj/mm/aaaa) : ");
+		String dateDeNaissance = scanner.nextLine();	
+		
+		// Stocker les informations dans un tableau de String
+        return new String[] { nom, prenom, dateDeNaissance };
+	}
 
 }
